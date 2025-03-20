c3a9c70fd9ff3349d2a24736882c819c6c24599f
Ldy <1790002826@qq.com>
2025-03-20 17:30:25 +0800

    更新优化--增加登录及去除部分无用代码

--- src/main/java/com/xinkao/erp/login/controller/AccountPasswordController.java
+++ src/main/java/com/xinkao/erp/login/controller/AccountPasswordController.java
@@ -32,50 +32,5 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/ap")
 @RestController
 public class AccountPasswordController extends BaseController{
-   
-//    @Resource
-//    private SysConfigService sysConfigService;
-//    @Resource
-//	private LoginService loginService;
-//    @Resource
-//    private LoginCaptchaService captchaService;
-//    /**
-//     * 生成验证码
-//     */
-//    @GetMapping("/captchaImage")
-//    public CaptchaResultVo getCode() {
-//        CaptchaResultVo resultVo = new CaptchaResultVo();
-//        //查看账号密码的验证码功能是否开放
-//        boolean captchaPicOnOff = sysConfigService.getCaptchaPicOnOff();
-//        resultVo.setCaptchaOnOff(String.valueOf(captchaPicOnOff));
-//        if (!captchaPicOnOff) {
-//        	resultVo.setCaptchaId("");
-//        	resultVo.setImg("");
-//            return resultVo;
-//        }
-//        //生成验证码
-//        GenCaptchaResultVo captchaResult = captchaService.genPicCaptcha();
-//        boolean captchaLowercaseOnOff = sysConfigService.getCaptchaLowercaseOnOff();
-//        String code = captchaResult.getCode();
-//        String codePic = captchaResult.getCodePic();
-//        // 保存验证码信息
-//        String uuid = IdUtil.simpleUUID();
-//        String verifyKey = XinKaoConstant.CAPTCHA_CODE_KEY + uuid;
-//        //判断大小写是否敏感
-//        if(captchaLowercaseOnOff) {
-//        	code = code.toLowerCase();
-//        }
-//        log.debug("生成的pic验证码是:{}",code);
-//        redisUtil.set(verifyKey, code, XinKaoConstant.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
-//        resultVo.setCaptchaId(uuid);
-//    	resultVo.setImg(codePic);
-//        return resultVo;
-//    }
-//   	/**
-//   	 * 登录接口
-//   	 */
-//   	@PostMapping("/login")
-//   	public BaseResponse login(ApLoginParam loginParam, HttpServletRequest request) {
-//   		return loginService.login(loginParam,request);
-//   	}
+
 }
