b6987298f5d2700df551b6f0c721c0a2da385266
bdqntea <bdqn_zzl@163.com>
2025-05-18 23:43:09 +0800

    提交修改

--- src/main/java/com/xinkao/erp/exercise/utils/jx2d/AttrData.java
+++ src/main/java/com/xinkao/erp/exercise/utils/jx2d/AttrData.java
@@ -18,15 +18,19 @@ public class AttrData {
         if (attrArray == null) {
             return;
         }
-        for (int i = 0; i < attrArray.size(); i++) {
-            JSONArray subArray = attrArray.getJSONArray(i);
-            List<String> innerList = new ArrayList<>();
-            for (int j = 0; j < subArray.size(); j++) {
-                innerList.add(subArray.getString(j));
-            }
+        List<String> innerList = new ArrayList<>();
+        if (attrArray.size()==1){
+            innerList.add(attrArray.getString(0));
             this.attr.add(innerList);
+        }else if (attrArray.size()>1){
+            for (int i = 0; i < attrArray.size(); i++) {
+                JSONArray subArray = attrArray.getJSONArray(i);
+                for (int j = 0; j < subArray.size(); j++) {
+                    innerList.add(subArray.getString(j));
+                }
+                this.attr.add(innerList);
+            }
         }
-
         JSONObject positionObj = obj.getJSONObject("position");
         this.position = new Position(positionObj);
 
