f64f9d1aef5936b9446b84cb5f1ea633943a6fd6
七七七 <1790002826@qq.com>
2025-08-28 11:06:54 +0800

    更新优化

--- src/main/java/com/xinkao/erp/exam/service/impl/ExamPageUserServiceImpl.java
+++ src/main/java/com/xinkao/erp/exam/service/impl/ExamPageUserServiceImpl.java
@@ -282,6 +282,9 @@ public class ExamPageUserServiceImpl extends BaseServiceImpl<ExamPageUserMapper,
                 .list();
         int allScores = 0;
         for (ExamPageUserAnswer examPageUserAnswer : examPageUserAnswers) {
+            if (StrUtil.isBlank(examPageUserAnswer.getUserAnswer())){
+                examPageUserAnswer.setUserAnswer("");
+            }
             //如果是单选题，判断是否正确,//填空题(暂时先按照全部正确才给分)
             if (100 == examPageUserAnswer.getShape() || 300 == examPageUserAnswer.getShape() || 700 == examPageUserAnswer.getShape() ){
                 if (examPageUserAnswer.getUserAnswer().equals(examPageUserAnswer.getRightAnswer())){
