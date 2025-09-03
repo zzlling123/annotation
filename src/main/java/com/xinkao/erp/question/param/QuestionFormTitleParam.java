3cd6c0943921e735d436439dbabb252103123c67
七七七 <1790002826@qq.com>
2025-09-03 19:50:44 +0800

    更新优化，添加题目标记

--- src/main/java/com/xinkao/erp/question/param/QuestionFormTitleParam.java
+++ src/main/java/com/xinkao/erp/question/param/QuestionFormTitleParam.java
@@ -22,7 +22,6 @@ public class QuestionFormTitleParam implements InputConverter<Question> {
 
     @ApiModelProperty("题目")
     @NotBlank(message = "题目不能为空")
-    @Size(min = 1, max = 255, message = "题目长度应在1到255个字符之间")
     private String question;
 
     @ApiModelProperty("排序")
