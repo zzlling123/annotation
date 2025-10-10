dd3173490bde2cc4b817d21babd0316a0460f3c7
bdqntea <bdqn_zzl@163.com>
2025-10-10 23:13:30 +0800

    提交修改

--- src/main/java/com/xinkao/erp/exercise/query/ExerciseRecordsVo.java
+++ src/main/java/com/xinkao/erp/exercise/query/ExerciseRecordsVo.java
@@ -24,61 +24,42 @@ public class ExerciseRecordsVo {
     private Date updateTime;
 
     private String remark;
-    @ApiModelProperty("关联到用户的ID")
     private Integer userId;
 
-    @ApiModelProperty("用户名")
     private String realName;
 
-    @ApiModelProperty("关联到练习模块的ID")
     private Integer moduleId;
 
-    @ApiModelProperty("shape 题目类型:100-单选 200-多选 300-填空 400-主观题 500-操作题")
     private Integer shape;
 
-    @ApiModelProperty("question_score 每道题的分数")
     private Integer questionScore;
 
-    @ApiModelProperty("练习开始时间")
     private LocalDateTime startTime;
 
-    @ApiModelProperty("练习结束时间")
     private LocalDateTime endTime;
 
-    @ApiModelProperty("练习时长分钟")
     private Long duration;
 
-    @ApiModelProperty("题目完成情况")
     private Integer completionStatus;
 
-    @ApiModelProperty("练习分数")
     private Integer score;
 
-    @ApiModelProperty("练习反馈信息")
     private String feedback;
 
-    @ApiModelProperty("正确标注个数")
     private Integer biao;
 
-    @ApiModelProperty("应该标注未标注个数")
     private Integer cuo;
 
-    @ApiModelProperty("错误标注个数")
     private Integer wu;
 
-    @ApiModelProperty("属性个数")
     private Integer shu;
 
-    @ApiModelProperty("总共需要标注个数")
     private Integer zong;
 
-    @ApiModelProperty("学生标注个数")
     private Integer da;
 
-    @ApiModelProperty("标注准确率 = biao / da")
     private BigDecimal accuracyRate;
 
-    @ApiModelProperty("标注准确覆盖率 = biao / zong")
     private BigDecimal coverageRate;
 
 }
--- src/main/java/com/xinkao/erp/exercise/query/InstantFeedbacksQuery.java
+++ src/main/java/com/xinkao/erp/exercise/query/InstantFeedbacksQuery.java
@@ -18,58 +18,44 @@ import java.util.List;
 @TableName("instant_feedbacks")
 public class InstantFeedbacksQuery  extends BasePageQuery implements Serializable {
 
-    @ApiModelProperty("关联到练习记录的ID")
     @TableField("record_id")
     private Integer recordId;
 
-    @ApiModelProperty("关联到题目的ID")
     @TableField("question_id")
     private Integer questionId;
 
-    @ApiModelProperty("标注类型")
     @TableField("type")
     private Integer type;
 
-    @ApiModelProperty("题目的标注类型")
     @TableField("shape")
     private Integer shape;
 
-    @ApiModelProperty("用户的答案")
     @TableField("user_answer")
     private String userAnswer;
 
-    @ApiModelProperty("用户得分")
     @TableField("user_score")
     private Integer userScore;
 
-    @ApiModelProperty("正确答案")
     @TableField("correct_answer")
     private String correctAnswer;
 
-    @ApiModelProperty("用户答案是否正确：0-错误 1-正确 2-部分正确 3-未作答")
     @TableField("is_correct")
     private Integer isCorrect;
 
-    @ApiModelProperty("状态:0-正常 1-删除")
     @TableField("is_del")
     private Integer isDel;
 
 
-    @ApiModelProperty("用户ID")
     private List<Integer> userId;
 
-    @ApiModelProperty("用户姓名")
     @TableField(exist = false)
     private String realName;
 
-    @ApiModelProperty("完成状态:1-进行中 2-已完成")
     @TableField("finished_state")
     private Integer finishedState;
 
-    @ApiModelProperty("查询开始时间")
     private String startTime;
 
-    @ApiModelProperty("查询结束时间")
     private String endTime;
 
 }
