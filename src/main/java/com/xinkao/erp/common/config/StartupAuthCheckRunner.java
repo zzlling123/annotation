9a8fad8eca0eced0643a3e6f56223710de621872
bdqntea <bdqn_zzl@163.com>
2025-10-10 23:06:59 +0800

    提交修改

--- src/main/java/com/xinkao/erp/common/config/StartupAuthCheckRunner.java
+++ src/main/java/com/xinkao/erp/common/config/StartupAuthCheckRunner.java
@@ -21,27 +21,19 @@ public class StartupAuthCheckRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
         String mainServerUrl = sysConfigService.getConfigByKey("device.authentication.server");
-        //截取mainServerUrl（http://114.67.238.43:10202/annotation）里面的ip
         String ip = DeviceUtils.extractIpFromUrl(mainServerUrl);
-        //获取设备的mac地址
         String macAddress = DeviceUtils.getMacAddress();
-        //获取当前设备的ip地址
         String ipAddress = DeviceUtils.getPublicIpAddress();
-        System.out.println("重启后请求内容开始");
-        System.out.println("当前设备是否与主服务器一致：" + ip.equals(ipAddress));
         if (!ip.equals(ipAddress)){
             String url = "http://114.67.238.43:10202/device/restartStatus?macAddress="+macAddress;
             if (mainServerUrl == null || mainServerUrl.isEmpty()) {
             }else{
-                // 2. 拼接主服务器接口
                 url = mainServerUrl + "/device/restartStatus?macAddress="+macAddress;
             }
             try {
                 restTemplate.getForObject(url, String.class);
-                // 可根据需要打印日志
                 System.out.println("远程授权校验请求已发送");
             } catch (Exception e) {
-                // 可根据需要打印日志
                 System.err.println("远程授权校验请求失败: " + e.getMessage());
             }
         }
